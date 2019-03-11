7fd229f1441356866aedba611fd0cf4e89b50921
Bozhidar Batsov <bozhidar@batsov.com>
2019-03-11 23:52:45 +0200

    Release 1.2

--- cider-eval-sexp-fu.el
+++ cider-eval-sexp-fu.el
@@ -6,7 +6,7 @@
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; Keywords: languages, clojure, cider
 ;; Created: 20 Mar 2015
-;; Version: 1.1
+;; Version: 1.2
 ;; Package-Requires: ((emacs "24") (eval-sexp-fu "0.5.0"))
 
 ;; This file is not part of GNU Emacs.
